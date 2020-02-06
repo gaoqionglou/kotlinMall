10854318a1bf98b80b9786d2988eb35b9d88af11
gaoqionglou <438279375@qq.com>
2020-02-06 23:27:10 +0800

    usercenter 个人中心模块的模块化

---
+++ usercenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.usercenter.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kotlin.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
