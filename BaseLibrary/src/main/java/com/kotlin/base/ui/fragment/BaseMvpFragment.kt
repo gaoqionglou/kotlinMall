91cd1692bee4c14edeea9586de3f90f0be638a8a
gaoqionglou <438279375@qq.com>
2020-02-11 00:52:44 +0800

    在BaseMvpFragment中dagger的注入放在onCreate中执行，放在onCreateView中执行会导致mPresenter无法注入(mPresenter=null)

--- BaseLibrary/src/main/java/com/kotlin/base/ui/fragment/BaseMvpFragment.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/fragment/BaseMvpFragment.kt
@@ -1,7 +1,11 @@
 package com.kotlin.base.ui.fragment
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import com.kotlin.base.common.BaseApplication
 import com.kotlin.base.injection.component.ActivityComponent
@@ -10,40 +14,72 @@ import com.kotlin.base.injection.module.ActivityModule
 import com.kotlin.base.injection.module.LifecyclerProviderModule
 import com.kotlin.base.presenter.BasePresenter
 import com.kotlin.base.presenter.view.BaseView
+import com.kotlin.base.widget.ProgressLoading
 import javax.inject.Inject
 
+/*
+    Fragment基类，业务相关
+ */
 abstract class BaseMvpFragment<T : BasePresenter<*>> : BaseFragment(), BaseView {
-    override fun showLoading() {
-
-    }
-
-    override fun hideLoading() {
-
-    }
-
-    override fun onError(errorTxt: String) {
-        Toast.makeText(activity, errorTxt, Toast.LENGTH_SHORT).show()
-    }
 
     @Inject
     lateinit var mPresenter: T
 
     lateinit var mActivityComponent: ActivityComponent
 
-    abstract fun initComponent()
+    private lateinit var mLoadingDialog: ProgressLoading
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initComponent()
         initActivityInjection()
+        initComponent()
+        //初始加载框
+        mLoadingDialog = ProgressLoading.create(context as Context)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    /*
+        Dagger注册
+     */
+    protected abstract fun initComponent()
+
+    /*
+        初始化Activity级别Component
+     */
     private fun initActivityInjection() {
         mActivityComponent = DaggerActivityComponent.builder()
             .appComponent((activity?.application as BaseApplication).appComponent)
             .activityModule(ActivityModule(activity as Activity))
             .lifecyclerProviderModule(LifecyclerProviderModule(this))
             .build()
+
     }
 
+    /*
+       显示加载框，默认实现
+    */
+    override fun showLoading() {
+        mLoadingDialog.showLoading()
+    }
+
+    /*
+        隐藏加载框，默认实现
+     */
+    override fun hideLoading() {
+        mLoadingDialog.hideLoading()
+    }
+
+    /*
+        错误信息提示，默认实现
+     */
+    override fun onError(errorTxt: String) {
+        Toast.makeText(activity, errorTxt, Toast.LENGTH_SHORT).show()
+    }
 }
